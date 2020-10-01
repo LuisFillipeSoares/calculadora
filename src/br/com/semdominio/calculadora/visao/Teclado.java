7967eb64aaecc4eba72c9b84f4323479d2e790ee
Luis Fillipe <jobluisfillipe@gmail.com>
2020-10-01 07:17:28 -0300

    Interface grafica com o GridBag

--- src/br/com/semdominio/calculadora/visao/Teclado.java
+++ src/br/com/semdominio/calculadora/visao/Teclado.java
@@ -1,9 +1,9 @@
 package br.com.semdominio.calculadora.visao;
 
 import java.awt.Color;
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
@@ -16,33 +16,56 @@ public class Teclado extends JPanel{ //aqui eu vou usar os botoes
 			
 	
 	public Teclado() {
-		setLayout(new GridLayout(5, 4)); //quantas linhas e quantas colunas de botoes no teclado
+		//setLayout(new GridLayout(5, 4)); //quantas linhas e quantas colunas de botoes no teclado
+		//o grid layout não permite botoes maiores que outros
+		GridBagLayout layout = new GridBagLayout(); 
+		GridBagConstraints constraints = new GridBagConstraints(); //recebe um x e y para linha e coluna
 		
-		add(new Botao("AC", COR_CINZA_ESCURO));
-		add(new Botao("+/-", COR_CINZA_ESCURO));
-		add(new Botao("%", COR_CINZA_ESCURO));
-		add(new Botao("/", COR_LARANJA));
+		setLayout(layout);
+		constraints.weightx = 1; //peso para ocupar os espaços
+		constraints.weighty = 1;
+		constraints.fill = GridBagConstraints.BOTH; //PRENCHER O BOTAO
 		
-		add(new Botao("7", COR_CINZA_CLARO));
-		add(new Botao("8", COR_CINZA_CLARO));
-		add(new Botao("9", COR_CINZA_CLARO));
-		add(new Botao("x", COR_LARANJA));
+		// linha 1
+		constraints.gridwidth = 3; //largura da linha para ocupar 3 linhas;
+		adicionarBotao("AC", COR_CINZA_ESCURO, constraints, 0, 0);
+		constraints.gridwidth = 1; //voltando pro peso inicial para que as demais não ocupem 3 linhas;
+		adicionarBotao("/", COR_LARANJA, constraints, 3, 0);
+	
+		// linha 2
+		adicionarBotao("7", COR_CINZA_CLARO, constraints, 0, 1);
+		adicionarBotao("8", COR_CINZA_CLARO, constraints, 1, 1);
+		adicionarBotao("9", COR_CINZA_CLARO, constraints, 2, 1);
+		adicionarBotao("x", COR_LARANJA, constraints, 3, 1);
 		
-		add(new Botao("4", COR_CINZA_CLARO));
-		add(new Botao("5", COR_CINZA_CLARO));
-		add(new Botao("6", COR_CINZA_CLARO));
-		add(new Botao("-", COR_LARANJA));
+		// linha 3
+		adicionarBotao("4", COR_CINZA_CLARO, constraints, 0, 2);
+		adicionarBotao("5", COR_CINZA_CLARO, constraints, 1, 2);
+		adicionarBotao("6", COR_CINZA_CLARO, constraints, 2, 2);
+		adicionarBotao("-", COR_LARANJA, constraints, 3, 2);
 		
-		add(new Botao("1", COR_CINZA_CLARO));
-		add(new Botao("2", COR_CINZA_CLARO));
-		add(new Botao("3", COR_CINZA_CLARO));
-		add(new Botao("+", COR_LARANJA));
+		// linha 4
+		adicionarBotao("1", COR_CINZA_CLARO, constraints, 0, 3);
+		adicionarBotao("2", COR_CINZA_CLARO, constraints, 1, 3);
+		adicionarBotao("3", COR_CINZA_CLARO, constraints, 2, 3);
+		adicionarBotao("+", COR_LARANJA, constraints, 3, 3);
 		
-		add(new Botao("0", COR_CINZA_CLARO));
-		add(new Botao("", COR_CINZA_CLARO));
-		add(new Botao(",", COR_CINZA_CLARO));
-		add(new Botao("=", COR_LARANJA));
+		// linha 5
+		constraints.gridwidth = 2;
+		adicionarBotao("0", COR_CINZA_CLARO, constraints, 0, 4);
+		constraints.gridwidth = 1;
+		adicionarBotao(",", COR_CINZA_CLARO, constraints, 2, 4);
+		adicionarBotao("=", COR_LARANJA, constraints, 3, 4);
 		
+	}
+
+
+
+	private void adicionarBotao(String texto, Color cor, GridBagConstraints constraints, int x, int y) { //metodo criado para ser possivel adicionar logo linha e coluna direto na instaciação de um novo botao
+		constraints.gridy = y;
+		constraints.gridx = x;
+		Botao botao = new Botao(texto, cor);
+		add(botao, constraints);	
 	}
 	
 }
